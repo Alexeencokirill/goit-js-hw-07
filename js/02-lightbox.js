3b047251d7810e61a413037b2a448be215422520
Alexeencokirill <97600028+Alexeencokirill@users.noreply.github.com>
2023-05-02 19:40:27 +0200

    task -2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery')
+const items = [];
+
+galleryItems.forEach(elem => {
+    const galleryLink = document.createElement('a')
+    galleryLink.className = 'gallery__link'
+    galleryLink.href = elem.original
+
+    const galleryImage = document.createElement('img')
+    galleryImage.className = 'gallery__image'
+    galleryImage.src = elem.preview
+    galleryImage.alt = elem.description
+    galleryImage.setAttribute('title', elem.description)
+
+    galleryLink.append(galleryImage)
+    items.push(galleryLink)
+
+})
+
+gallery.append(...items)
+
+new SimpleLightbox('.gallery a', { captionDelay: 250 });
+
+console.log(document);
